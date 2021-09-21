704d945cd8b073bffce63d0fb0350a45ea226d1c
Gleb Varganov <webgleb@gmail.com>
2021-09-22 03:13:25 +0500

    Add getInfo method

--- src/Api/Key.php
+++ src/Api/Key.php
@@ -4,7 +4,17 @@
 namespace Devgleb\CoinMarketCap\Api;
 
 
-class Key
+class Key extends Api
 {
-
+    /**
+     * Returns API key details and usage stats.
+     * @link https://coinmarketcap.com/api/documentation/v1/#tag/key
+     *
+     * @return array
+     * @throws \Exception
+     */
+    public function getInfo(): array
+    {
+        return $this->get('/key/info');
+    }
 }
